e450e38b04f5af11f748ae155147c6e309ff9939
Predrag <pedja.antic97@gmail.com>
2019-12-25 19:43:11 +0100

    Added comments and bookmarks entities with controller

---
+++ Entities/BookmarkNews.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineNews.Entities
+{
+    public class BookmarkNews
+    {
+        public string username { get; set; }
+        public string newsID { get; set; }
+        public string title { get; set; }
+        public string imageURL { get; set; }
+        public string description { get; set; }
+        public string date_of_publication { get; set; }
+        public string journalist { get; set; }
+    }
+}
