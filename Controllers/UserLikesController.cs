22e32e1a4bf189ffb06b31665d3533657fe117ba
zbalanovic <z.balanovic@elfak.rs>
2019-12-26 21:20:59 +0100

    Like/Dislike

---
+++ Controllers/UserLikesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dse;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineNews.Entities;
+
+namespace OnlineNews.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserLikesController : ControllerBase
+    {
+        // GET: api/UserLikes/5
+        //[HttpGet("{username}")]
+        //public string Get(string username)
+        //{
+           
+        //}
+
+        // POST: api/UserLikes
+        [HttpPost]
+        public ActionResult Post([FromBody]UserLikes value)
+        {
+            Dse.ISession session = SessionManager.GetSession();
+            if (session != null)
+            {
+                Row hasLiked = session.Execute("select count(*), username, \"newsID\", \"isLike\" from \"UserLikes\" where username = '" + value.Username + "' and \"newsID\" =" + "'" + value.NewsID + "'").FirstOrDefault();
+                if (Int32.Parse(hasLiked["count"].ToString()) == 0)
+                {
+                    session.Execute("insert into \"UserLikes\"(\"username\", \"newsID\", \"isLike\") values('" + value.Username + "', '" + value.NewsID + "', " + value.IsLike + ")");
+                    if (value.IsLike)
+                    {
+                        session.Execute("update \"OnlineNews\".\"News\" set likes = " + (value.NumberOfLikes + 1)+ " where \"newsID\" = '" + value.NewsID + "'");
+                        return StatusCode(200);
+                    }
+                    else
+                    {
+                        session.Execute("update \"OnlineNews\".\"News\" set dislikes = " + (value.NumberOfDislikes + 1)+ " where \"newsID\" = '" + value.NewsID + "'");
+                        return StatusCode(200);
+                    }
+                }
+                else if(value.IsLike)
+                {
+                    if (Boolean.Parse(hasLiked["isLike"].ToString()))
+                    {
+                        session.Execute("delete from \"UserLikes\" where username = '" + value.Username + "' and \"newsID\" ='" + value.NewsID + "'");
+                        session.Execute("update \"OnlineNews\".\"News\" set likes = " + (value.NumberOfLikes - 1) + " where \"newsID\" = '" + value.NewsID + "'");
+                        return StatusCode(200);
+                    }
+                    else
+                    {
+                        session.Execute("update \"OnlineNews\".\"UserLikes\" set \"isLike\" = " + true + " where username = '" + value.Username + "' and \"newsID\" ='" + value.NewsID + "'");
+                        session.Execute("update \"OnlineNews\".\"News\" set likes = " + (value.NumberOfLikes + 1)+ ", dislikes = " + (value.NumberOfDislikes - 1)+ " where \"newsID\" = '" + value.NewsID + "'");
+                        return StatusCode(200);
+                    }
+                }
+                else if (!value.IsLike)
+                {
+                    if (!Boolean.Parse(hasLiked["isLike"].ToString()))
+                    {
+                        session.Execute("delete from \"UserLikes\" where username = '" + value.Username + "' and \"newsID\" ='" + value.NewsID + "'");
+                        session.Execute("update \"OnlineNews\".\"News\" set dislikes = " + (value.NumberOfDislikes - 1)+ " where \"newsID\" = '" + value.NewsID + "'");
+                        return StatusCode(200);
+                    }
+                    else
+                    {
+                        session.Execute("update \"OnlineNews\".\"UserLikes\" set \"isLike\" = " + false + " where username = '" + value.Username + "' and \"newsID\" ='" + value.NewsID + "'");
+                        session.Execute("update \"OnlineNews\".\"News\" set likes = " + (value.NumberOfLikes - 1)+ ", dislikes = " + (value.NumberOfDislikes + 1)+ " where \"newsID\" = '" + value.NewsID + "'");
+                        return StatusCode(200);
+                    }
+                }
+            }
+            return StatusCode(500);
+        }
+
+    }
+}
